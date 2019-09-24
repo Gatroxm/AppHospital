72eb0cbb0f37d5d64c1c99edf7cd5f042308dc37
Gatroxm <tavoxpau@gmail.com>
2019-09-23 21:48:21 -0500

    Se crea el proyecto con la estructura basica y el rutero

---
+++ src/app/shared/no-page-fond/no-page-fond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-fond',
+  templateUrl: './no-page-fond.component.html',
+  styles: []
+})
+export class NoPageFondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
